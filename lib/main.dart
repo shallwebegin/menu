5b059702612255834969197a1a6a4d1163ff859e
Berke Kartal <shallwebegin@users.noreply.github.com>
2023-12-22 19:52:21 +0300

    add meals

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:menu/categories.dart';
+import 'package:menu/data/dummy_data.dart';
+import 'package:menu/screens/categories.dart';
+import 'package:menu/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const MealsScreen(title: 'Some Category...',meals: dummyMeals),
     );
   }
 }
