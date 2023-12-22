263651dfee94386d4ead4fda435b98765d2063cc
Berke Kartal <shallwebegin@users.noreply.github.com>
2023-12-22 14:31:27 +0300

    add gridview

--- lib/categories.dart
+++ lib/categories.dart
@@ -4,10 +4,42 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Categories'),
+        title: const Text('Pick your Category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: const [
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
